77fa9ad43adb581929fe84ef42108775f9ed1b67
Aakash <aakash.codes02@gmail.com>
2024-09-03 08:51:23 +0530

    [Fundamentals-1] Assignment on Values & Variables

---
+++ 01-Fundamentals-1/assignments.js
@@ -0,0 +1,23 @@
+// ================ VALUES & VARIABLES ================
+
+/*
+const country = 'India';
+const continent = 'Asia';
+const population = 1.42;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(
+  country +
+    ' is a part of ' +
+    continent +
+    " and it's population is " +
+    population +
+    ' million.'
+);
+*/
+
+// ============================================
+// ================ DATA TYPES ================
