9da2cddcb5c4e53bae46394de6255ad275233d8d
Aakash <aakash.codes02@gmail.com>
2024-09-03 11:15:46 +0530

    [Fundamentals-1] Completed Challenge-1: BMI of Mark and John

---
+++ 01-Fundamentals-1/challenges.js
@@ -0,0 +1,31 @@
+// ================ CHALLENGE-1 ================
+
+/*
+// DATA 1:
+// Mark weight = 78
+// Mark height = 1.69
+// John weight = 92
+// John height = 1.95
+
+// DATA 2:
+// Mark weight = 95
+// Mark height = 1.88
+// John weight = 85
+// John height = 1.76
+
+const massMark = 95;
+const heightMark = 1.88;
+const BMIMark = (massMark / heightMark) * heightMark;
+
+const massJohn = 85;
+const heightJohn = 1.76;
+const BMIJohn = (massJohn / heightJohn) * heightJohn;
+
+console.log('BMI of Mark:', BMIMark);
+console.log('BMI of John:', BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+*/
+
+// ===========================================
