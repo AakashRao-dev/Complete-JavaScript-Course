b72eb1ca28b273cd93fa3b2726f50c75b2525f69
Aakash <aakash.codes02@gmail.com>
2024-09-04 12:55:35 +0530

    [Fundamentals-1] Assignment on switch case statement

--- 01-Fundamentals-1/assignments.js
+++ 01-Fundamentals-1/assignments.js
@@ -172,3 +172,28 @@ if (language === 'English' && population < 50 && !isIsland) {
 
 // ==========================================================
 // ================== THE SWITCH STATEMENT ==================
+
+/*
+const language = 'Hindi';
+
+switch (language) {
+  case 'Chinese':
+  case 'Mandarin':
+    console.log('MOST number of native speakers!');
+    break;
+  case 'Spanish':
+    console.log('2nd number in place of native speakers');
+    break;
+  case 'English':
+    console.log('3rd place');
+    break;
+  case 'Hindi':
+    console.log('Number 4');
+    break;
+  case 'Arabic':
+    console.log("5'th most spoken language");
+    break;
+  default:
+    console.log('Great language too :D');
+}
+*/
