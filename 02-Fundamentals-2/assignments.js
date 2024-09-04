a090198548af344a207e0d8d3f2426d32255feb2
Aakash <aakash.codes02@gmail.com>
2024-09-04 23:47:20 +0530

    [Fundamentals-2] Assignment on Function Declarations vs. Expressions

--- 02-Fundamentals-2/assignments.js
+++ 02-Fundamentals-2/assignments.js
@@ -14,3 +14,20 @@ console.log(malaysia);
 console.log(singapore);
 console.log(taiwan);
 */
+
+// ========================================================
+// ========== FUNCTION DECLARATION VS EXPRESSION ==========
+
+/*
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const malaysiaPopPercent = percentageOfWorld1(33.4);
+const chinaPopPercent = percentageOfWorld1(1441);
+const taiwanPopPercent = percentageOfWorld1(23.4);
+
+console.log(malaysiaPopPercent);
+console.log(chinaPopPercent);
+console.log(taiwanPopPercent);
+*/
