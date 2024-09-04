c5a7dc082e53aa53c66ea8f264d517a7e4961e1f
Aakash <aakash.codes02@gmail.com>
2024-09-04 20:50:33 +0530

    [Fundamentals-1] Introduction to Functions

--- 02-Fundamentals-2/scripts.js
+++ 02-Fundamentals-2/scripts.js
@@ -16,5 +16,37 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const private = 30;
 */
 
-// ===================================================
+// =================================================
 // =================== FUNCTIONS ===================
+
+// Function:
+// A piece of code that we can reuse over & over again in our code.
+// It's like a variable but for whole chunks of code.
+// Allows us to write more maintainable code by allowing to create resusable block of code - DRY
+
+/*
+function logger() {
+  console.log('My name is Aakash');
+}
+
+// calling / invoking / running function
+logger(23);
+logger();
+logger();
+
+// parameters: only specific to functions
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges...`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+// ==========================================================
+// ========== FUNCTION DECLARATION VS. EXPRESSIONS ==========
