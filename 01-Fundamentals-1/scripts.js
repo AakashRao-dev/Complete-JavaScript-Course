b343d749e5f64324b75ce14535449e1e6028e107
Aakash <aakash.codes02@gmail.com>
2024-09-04 11:03:24 +0530

    [Fundamentals-1] Truthy & Falsy values

--- 01-Fundamentals-1/scripts.js
+++ 01-Fundamentals-1/scripts.js
@@ -145,3 +145,34 @@ console.log(n);
 
 // =============================================================
 // =================== TRUTHY & FALSY VALUES ===================
+
+/*
+// 5 falsy values
+// 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Aakash'));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean({}));
+
+// Logical context, Logical Operators
+
+const money = 100;
+if (money) {
+  console.log(`Don't spend it all ;)`);
+} else {
+  console.log(`You should get a job!`);
+}
+
+let height = 0;
+if (height) {
+  console.log(`Yay, height is defined!`);
+} else {
+  console.log(`Height is UNDEFINED :(`);
+}
+*/
+
+// ======================================================================
+// =================== EQUALITY OPERATORS: == vs. === ===================
