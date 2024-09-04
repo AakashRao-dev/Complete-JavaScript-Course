618bd1e01163c70654620152661de9418b142326
Aakash <aakash.codes02@gmail.com>
2024-09-04 23:40:55 +0530

    [Fundamentals-2] Assignment on Functions

---
+++ 02-Fundamentals-2/assignments.js
@@ -0,0 +1,16 @@
+// =================================================
+// =================== FUNCTIONS ===================
+
+/*
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} people and its capital city is ${capitalCity}.`;
+}
+
+const malaysia = describeCountry('Malaysia', 33.4, 'Kuala Lumpur');
+const singapore = describeCountry('Singapore', 5.92, 'Singapore');
+const taiwan = describeCountry('Taiwan', 23.4, 'Taipei');
+
+console.log(malaysia);
+console.log(singapore);
+console.log(taiwan);
+*/
