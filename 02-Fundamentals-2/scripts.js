bbd1b5b1696595da72661bc38289fc0123b6f8e5
Aakash <aakash.codes02@gmail.com>
2024-09-04 15:48:58 +0530

    [Fundamentals-2] Strict Mode

---
+++ 02-Fundamentals-2/scripts.js
@@ -0,0 +1,20 @@
+'use strict';
+// ===================================================
+// =================== STRICT MODE ===================
+
+/*
+// helps with finding bugs on if-else block
+// helps with avoid using reserved words
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 30;
+*/
+
+// ===================================================
+// =================== FUNCTIONS ===================
