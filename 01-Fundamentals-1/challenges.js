fc5b28bd5ba329aa4a8fabc6720a2c1b6eca04df
Aakash <aakash.codes02@gmail.com>
2024-09-04 13:12:49 +0530

    [Fundamentals-1] Completed Challenge-4: Calculating Bill & Tip values with ternary operator

--- 01-Fundamentals-1/challenges.js
+++ 01-Fundamentals-1/challenges.js
@@ -68,3 +68,18 @@ if (scoreDolphins > scoreKoalas) {
   console.log('Koalas win the trophy');
 }
 */
+
+// =============================================
+// ================ CHALLENGE-4 ================
+
+// DATA:
+// bill = 275, 40, 430
+
+/*
+const bill = 275;
+const tip = bill > 50 && bill < 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
+);
+*/
