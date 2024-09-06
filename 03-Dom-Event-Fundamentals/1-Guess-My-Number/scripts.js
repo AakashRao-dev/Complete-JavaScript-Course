6147f07fc78ab0136a45452b0803b67cb625ba66
Aakash <aakash.codes02@gmail.com>
2024-09-06 10:37:32 +0530

    [Dom & Event Fundamentals] Project-01: Added the highScore Functionality

--- 03-Dom-Event-Fundamentals/1-Guess-My-Number/scripts.js
+++ 03-Dom-Event-Fundamentals/1-Guess-My-Number/scripts.js
@@ -15,14 +15,17 @@
 'use strict';
 const checkBtn = document.querySelector('.btn.check');
 const againBtn = document.querySelector('.btn.again');
-let guess = Number(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber); // remove this
 let score = 20;
+let highscore = 0;
 
 checkBtn.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+    console.log(guess);
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
@@ -30,6 +33,11 @@ checkBtn.addEventListener('click', function () {
     // MANIPULATE CSS STYLES
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -54,11 +62,12 @@ checkBtn.addEventListener('click', function () {
 againBtn.addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber); // remove this
 
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.score').textContent = score;
-  guess = '';
+  document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
