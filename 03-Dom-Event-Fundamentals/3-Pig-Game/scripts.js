6a463751ccacffaa9e68a53c8f32b6c6c89eeeb4
Aakash <aakash.codes02@gmail.com>
2024-09-12 19:23:20 +0530

    [Dom & Event Fundamentals] Project-03: Switch Player & Modal Functionaliy added

--- 03-Dom-Event-Fundamentals/3-Pig-Game/scripts.js
+++ 03-Dom-Event-Fundamentals/3-Pig-Game/scripts.js
@@ -1,12 +1,17 @@
 'use strict';
 
 // Selecting DOM Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 
 const currentPlayer0El = document.getElementById('current--0');
 const currentPlayer1El = document.getElementById('current--1');
@@ -16,7 +21,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice
 btnRoll.addEventListener('click', function () {
@@ -31,8 +39,23 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    currentPlayer0El.textContent = currentScore; // TODO - CHANGE THIS
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
+
+// MODAL FUNCTIONALITY FOR INSTRUCTIONS
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
