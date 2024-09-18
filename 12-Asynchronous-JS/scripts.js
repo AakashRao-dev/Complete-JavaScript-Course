bf2fceb11ab5d337b6fbb6584bf726992e370bb4
Aakash <aakash.codes02@gmail.com>
2024-09-18 21:40:54 +0530

    [Async JS] Chaining Promises with .then()

--- 12-Asynchronous-JS/scripts.js
+++ 12-Asynchronous-JS/scripts.js
@@ -226,3 +226,55 @@ const getCountryData = function (country) {
 
 getCountryData('India');
 */
+
+// ============================================================
+// ===================== CHAINING PROMISES ====================
+
+/*
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} Million</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+        </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  // COUNTRY-1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // COUNTRY-2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    // ONE CALLBACK - ONE RETURN PROMISE - THEN CHAIN THEN FOR RESPONSE to avoid callback hell
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+    });
+};
+
+getCountryData('India');
+*/
+
+// ==============================================================
